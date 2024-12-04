320798be0ef960b772d826ac7c806c8743515b57
inunice <ammanguan@up.edu.ph>
2024-12-05 01:32:52 +0800

    feat: app sidebar

---
+++ front-end/src/app/live-feed/page.tsx
@@ -0,0 +1,7 @@
+export default function LiveFeed() {
+  return (
+    <div>
+      <h1>Live Feed</h1>
+    </div>
+  );
+}
---
+++ front-end/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Bantai</h1>
+    </div>
+  );
+}
